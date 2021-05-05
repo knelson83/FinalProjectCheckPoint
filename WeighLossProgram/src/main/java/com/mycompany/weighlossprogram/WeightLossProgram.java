4525d1c20ab6e33cd2b37eba79d560afbfed376c
Kiwanda Nelson <77902208+knelson83@users.noreply.github.com>
2021-05-05 07:41:43 -0500

    FinalProject

--- WeighLossProgram/src/main/java/com/mycompany/weighlossprogram/WeightLossProgram.java
+++ WeighLossProgram/src/main/java/com/mycompany/weighlossprogram/WeightLossProgram.java
@@ -7,12 +7,16 @@ import javax.swing.JOptionPane;
  * @author kiwin
  */
 public class WeightLossProgram {
+
 	private String UserName;
 	private int UserAge;
-	private float UserWeight, UserHeight, GoalWeight;
-	private int MilesWalked;
+	private double UserWeight, GoalWeight;
+	private double MilesWalked;
 	String input;
 
+	public WeightLossProgram(String UserName, int Userage, double UserWeight, double GoalWeigh,double MilesWalked){
+		
+	}
 	public String getUserName() {
 		return UserName;
 	}
@@ -29,38 +33,29 @@ public class WeightLossProgram {
 		this.UserAge = UserAge;
 	}
 
-	public float getUserWeight() {
+	public double getUserWeight() {
 		return UserWeight;
 	}
 
-	public void setUserWeight(float UserWeight) {
+	public void setUserWeight(double UserWeight) {
 		this.UserWeight = UserWeight;
 	}
 
-	public float getUserHeight() {
-		return UserHeight;
-	}
-
-	public void setUserHeight(float UserHeight) {
-		this.UserHeight = UserHeight;
-	}
-
-	public float getGoalWeight() {
+	public double getGoalWeight() {
 		return GoalWeight;
 	}
 
-	public void setGoalWeight(float GoalWeight) {
+	public void setGoalWeight(double GoalWeight) {
 		this.GoalWeight = GoalWeight;
 	}
 
-	public int getMilesWalked() {
+	public double getMilesWalked() {
 		return MilesWalked;
 	}
 
-	public void setMilesWalked(int MilesWalked) {
+	public void setMilesWalked(double MilesWalked) {
 		this.MilesWalked = MilesWalked;
 	}
-
 
 	public String getInput() {
 		return input;
@@ -69,36 +64,35 @@ public class WeightLossProgram {
 	public void setInput(String input) {
 		this.input = input;
 	}
-	
-	public static void main (String[] args){
-		
-		
-	String response;
-	response = JOptionPane.showInputDialog(" Enter your name: ");
-	String name = response;
-	
-	response = JOptionPane.showInputDialog(" Enter your age: ");
-		int age = Integer.parseInt(response); 
-		
-	response = JOptionPane.showInputDialog(" Enter your weight: ");
-	 float weight = Float.parseFloat(response);
-	 
-	 response = JOptionPane.showInputDialog(" Enter your height: ");
-	 float height = Float.parseFloat(response);
-	 
-	 response = JOptionPane.showInputDialog(" Enter your goal weight: ");
-	 float goalweight = Float.parseFloat(response);
-	
-	response = JOptionPane.showInputDialog(" Walking 5miles per day is equal to one l pound of weight loss. Enter number of miles walked today: " );
-	 int miles  = Integer.parseInt(response);
-	
-	
-	
-	
-	
-	
-	
-	
-}
-	
+
+	public static void main(String[] args) {
+
+		String response;
+		response = JOptionPane.showInputDialog(" Enter your name: ");
+
+		String name = response;
+
+		response = JOptionPane.showInputDialog(" Enter your age: ");
+		int age = Integer.parseInt(response);
+
+		response = JOptionPane.showInputDialog(" Enter your weight: ");
+		double weight = Double.parseDouble(response);
+
+		response = JOptionPane.showInputDialog(" Enter your goal weight: ");
+		double goalweight = Double.parseDouble(response);
+
+		response = JOptionPane.showInputDialog(" Walking 5miles per day "
+			+ "equals l pound of weight loss. Enter number "
+			+ "of miles walked today: ");
+		double miles = Double.parseDouble(response);
+
+		String message = "Welcome " + name +"!" + " your current weight is "
+			+ weight + " and your goalweight is " 
+			+ goalweight + " you need to walk"
+			+ " 5 miles per day to reach your goal weight.";
+
+		System.out.println(message);
+
+
+	}
 }
